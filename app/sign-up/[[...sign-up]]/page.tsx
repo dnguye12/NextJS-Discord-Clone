c3726fde65d2e2879190f0a346afcded4f216c9b
dnguye12 <duchuyng051@gmail.com>
2025-01-14 11:45:22 +0100

    clerk + database steup

---
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignUp } from "@clerk/nextjs";
+
+function Page() {
+    return (
+        <div className="w-full h-full flex justify-center items-center"><SignUp /></div>
+    )
+
+}
+
+export default Page
