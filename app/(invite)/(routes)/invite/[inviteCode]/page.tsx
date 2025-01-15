bdbeb67f28f92a005fb2a9de2c03f00b4374d4d2
dnguye12 <duchuyng051@gmail.com>
2025-01-15 20:56:51 +0100

    server invite

---
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -0,0 +1,60 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+interface InviteCodePageProps {
+    params: {inviteCode: string;}
+}
+
+const InviteCodePage = async ({ params }: InviteCodePageProps) => {
+    const profile = await currentProfile()
+    const { redirectToSignIn } = await auth()
+    const {inviteCode} = await params
+
+    if (!profile) {
+        return redirectToSignIn()
+    }
+
+    if (!inviteCode) {
+        return redirect("/")
+    }
+
+    const existingServer = await db.server.findFirst({
+        where: {
+            inviteCode: inviteCode,
+            members: {
+                some: {
+                    profileId: profile.id
+                }
+            }
+        }
+    })
+
+    if(existingServer) {
+        return redirect(`/servers/${existingServer.id}`)
+    }
+
+    const server = await db.server.update({
+        where: {
+            inviteCode: inviteCode
+        },
+        data: {
+            members: {
+                create: [
+                    {
+                        profileId: profile.id
+                    }
+                ]
+            }
+        }
+    })
+
+    if(server) {
+        return redirect(`/servers/${server.id}`)
+    }
+
+    return null
+}
+
+export default InviteCodePage
---
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -0,0 +1,50 @@
+import { ServerSidebar } from "@/components/server/server-sidebar";
+import { currentProfile } from "@/lib/current-profile"
+import { db } from "@/lib/db"
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation";
+
+interface ServerIdLayoutProps {
+    children: React.ReactNode;
+    params: { serverId: string }
+}
+
+const ServerIdLayout = async ({ children, params }: ServerIdLayoutProps) => {
+    const profile = await currentProfile()
+    const { redirectToSignIn } = await auth()
+    const { serverId } = await params
+
+    if (!profile) {
+        return redirectToSignIn()
+    }
+
+    const server = await db.server.findUnique({
+        where: {
+            id: serverId,
+            members: {
+                some: {
+                    profileId: profile.id
+                }
+            }
+        }
+    })
+
+    if (!server) {
+        return redirect("/")
+    }
+
+    return (
+        <div className="h-full">
+            <div className="hidden md:flex fixed h-full w-60 z-20 flex-col inset-y-0">
+                <ServerSidebar
+                    serverId={serverId}
+                />
+            </div>
+            <main className="h-full md:pl-60">
+                {children}
+            </main>
+        </div>
+    )
+}
+
+export default ServerIdLayout
