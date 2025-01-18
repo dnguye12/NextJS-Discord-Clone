90b27627a179642ea4f3d21dca81f01108d80539
dnguye12 <duchuyng051@gmail.com>
2025-01-18 10:45:55 +0100

    create new channels

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -51,9 +51,9 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
                 }
                 {
                     isAdmin && (
-                        <DropdownMenuItem 
-                        onClick={() => onOpen("members", { server })}
-                        className="px-3 py-2 text-sm cursor-pointer"
+                        <DropdownMenuItem
+                            onClick={() => onOpen("members", { server })}
+                            className="px-3 py-2 text-sm cursor-pointer"
                         >
                             Manage Members
                             <Users className="h-4 w-4 ml-auto" />
@@ -62,7 +62,9 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
                 }
                 {
                     isModerator && (
-                        <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+                        <DropdownMenuItem
+                            onClick={() => onOpen("createChannel", { server })}
+                            className="px-3 py-2 text-sm cursor-pointer">
                             Create Channel
                             <PlusCircle className="h-4 w-4 ml-auto" />
                         </DropdownMenuItem>
