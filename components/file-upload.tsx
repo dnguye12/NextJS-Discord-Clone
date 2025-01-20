d0df6660ca8b32a63bb9971566831ffcf246eb7f
dnguye12 <duchuyng051@gmail.com>
2025-01-20 23:26:25 +0100

    message attachments

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,6 +1,7 @@
 import { UploadDropzone } from "@/lib/uploadthing";
-import { X } from "lucide-react";
+import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 interface FileUploadProps {
     onChange: (url?: string) => void;
@@ -9,7 +10,12 @@ interface FileUploadProps {
 }
 
 const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
-    const fileType = value?.split(".").pop()
+    const [uploadName, setUploadName] = useState("")
+    const [fileType, setFileType] = useState("")
+
+    useEffect(() => {
+        setFileType(uploadName?.split(".").pop() || "")
+    }, [uploadName])
 
     if (value && fileType !== "pdf") {
         return (
@@ -30,11 +36,40 @@ const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
             </div>
         )
     }
+
+    if (value && fileType === "pdf") {
+        return (
+            <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+                <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+                <a
+                    href={value}
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+                >
+                    {uploadName}
+                </a>
+
+                <button
+                    onClick={() => {
+                        onChange("")
+                        setUploadName("")
+                    }}
+                    className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
+                    type="button"
+                >
+                    <X className="h-4 w-4" />
+                </button>
+            </div>
+        );
+    }
+
     return (
         <UploadDropzone
             endpoint={endpoint}
             onClientUploadComplete={(res) => {
                 onChange(res?.[0].url)
+                setUploadName(res?.[0]?.name);
             }}
             onUploadError={(error: Error) => {
                 console.log(error)
